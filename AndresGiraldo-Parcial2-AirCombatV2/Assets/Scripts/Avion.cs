d57007586b6d62460354e1962edfd522a82dd873
Andres Giraldo <andresgir0390@gmail.com>
2018-04-18 20:04:01 -0500

    Parcial 2

---
+++ AndresGiraldo-Parcial2-AirCombatV2/Assets/Scripts/Avion.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Avion : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//Movemos hacia arribba
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			this.transform.Translate (0.0f, 2.0f, 0.0f);
+		}
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			this.transform.Translate (0.0f, -2.0f, 0.0f);
+		}
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			this.transform.Translate (0.0f, 0.0f, -2.0f);
+		}
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			this.transform.Translate (0.0f, 0.0f, 2.0f);
+		}
+
+
+		//Tecla espacio presionada
+		if (Input.GetKeyDown (KeyCode.Space)) {
+
+			//Reconstruyo un gameObject a partir
+			//de la informacion guardada en un prefab 
+
+			GameObject missile = GameObject.Instantiate (Resources.Load ("Prefabs/Missile") as GameObject);
+
+			missile.transform.SetParent (this.gameObject.transform);
+			missile.transform.localPosition = new Vector3 (0.03f, 1.59f, 2.47f);
+			missile.transform.SetParent (null);
+			 
+		}
+	}
+}
---
+++ AndresGiraldo-Parcial2-AirCombatV2/Assets/Scripts/Missile.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missile : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//mover hacia adelante
+		this.transform.Translate (0.0f, 0.0f, 8.0f);
+
+	}
+
+	void OnCollisionEnter(Collision collision) { 
+
+		if (collision.collider.gameObject.tag == "MarsAlien") {
+
+			//carga el prefab de una explosion
+			GameObject explosion = GameObject.Instantiate (Resources.Load ("Prefabs/MarsExplosion") as GameObject);
+
+			//Muevo la explosion
+			explosion.transform.position = collision.collider.gameObject.transform.position;
+
+			//Eliminamos el piso
+			Destroy (collision.collider.gameObject);
+
+			//Destruye el misil
+			Destroy (this.gameObject);
+		
+		}
+
+			if (collision.collider.gameObject.tag == "JupiterAlien") {
+
+				//carga el prefab de una explosion
+				GameObject explosion = GameObject.Instantiate (Resources.Load ("Prefabs/JupiterExplosion") as GameObject);
+
+				//Muevo la explosion
+				explosion.transform.position = collision.collider.gameObject.transform.position;
+
+				//Eliminamos el piso
+				Destroy (collision.collider.gameObject);
+
+				//Destruye el misil
+				Destroy (this.gameObject);
+
+			}
+			
+
+	if (collision.collider.gameObject.tag == "NeptuneAlien") {
+
+		//carga el prefab de una explosion
+		GameObject explosion = GameObject.Instantiate (Resources.Load ("Prefabs/NeptuneExplosion") as GameObject);
+
+		//Muevo la explosion
+		explosion.transform.position = collision.collider.gameObject.transform.position;
+
+		//Eliminamos el piso
+		Destroy (collision.collider.gameObject);
+
+		//Destruye el misil
+		Destroy (this.gameObject);
+
+	}
+}
+}
+
+
